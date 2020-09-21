873f2fbf0af913585c1566a9cf03347c93b9b78b
Nate <nigllemmesmash@gmail.com>
2020-09-21 16:12:18 +0300

    Initial commit

---
+++ src/ru/mirea/inbo0219_Shape/Shape.java
@@ -0,0 +1,11 @@
+package ru.mirea.inbo0219_Shape;
+
+public abstract class Shape {
+    abstract void myMethod();
+}
+
+class Water extends Shape {
+    void myMethod(){
+        int i = 5;
+    }
+}
